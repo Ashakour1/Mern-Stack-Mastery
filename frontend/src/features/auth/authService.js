ea068803aa5496f765d400601bfd7fbc9b77410c
Ashakuur1 <engshakrayare114@gmail.com>
2024-01-28 16:24:49 +0300

    login and logout

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -1,19 +1,36 @@
 import axios from "axios";
 
-const API_URL = "/api/users";
+const API_URL = "/api/users/";
 
 // reg user
 const register = async (userData) => {
   const response = await axios.post(API_URL, userData);
-
+  // console.log(userData);
+  // console.log(response.data.token);
   if (response.data) {
-    localStorage.setItem("user", JSON.stringify(response.data));
-    return response.data;
+    localStorage.setItem("user", JSON.stringify(response.data.token));
+    return response.data.token;
+
+  }
+};
+const login = async (userData) => {
+  const response = await axios.post(API_URL + 'login', userData);
+  // console.log(userData);
+  // console.log(response.data.token);
+  if (response.data) {
+    localStorage.setItem("user", JSON.stringify(response.data.token));
+    return response.data.token;
+
   }
 };
 
+const logout = () => {
+  localStorage.removeItem("user");
+}
 const AuthService = {
   register,
+  logout,
+  login,
 };
 
 export default AuthService;
